e3e4e883d2f64d029001e47351485e833d975fb7
Muhammad Noamany <muhammadnoamany@gmail.com>
2024-07-06 20:43:07 +0300

    # auth interceptors

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -3,17 +3,21 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class ForceJsonResponse
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
     public function handle($request, Closure $next)
     {
-        $request->headers->set('Accept', 'application/json');
+        if (Str::startsWith($request->path(), 'api')) {
+            $request->headers->set('Accept', 'application/json');
+        }
         return $next($request);
     }
 }
