28ba70a0d6a4f82a810934120b317738f2855b93
Muhammad Noamany <muhammadnoamany@gmail.com>
2024-09-29 16:31:05 +0300

    # notifications api

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
@@ -9,46 +10,8 @@ class NotificationsController extends Controller
     //
     public function index(Request $request)
     {
-        $data = [
-            'data' => [
-                [
-                    'image' => 'https://static.vecteezy.com/system/resources/previews/009/394/762/original/bell-icon-transparent-notification-free-png.png',
-                    'title' => 'test',
-                    'content' => 'test notification content'
-                ],
-                [
-                    'image' => 'https://static.vecteezy.com/system/resources/previews/009/394/762/original/bell-icon-transparent-notification-free-png.png',
-                    'title' => 'test',
-                    'content' => 'test notification content'
-                ], [
-                    'image' => 'https://static.vecteezy.com/system/resources/previews/009/394/762/original/bell-icon-transparent-notification-free-png.png',
-                    'title' => 'test',
-                    'content' => 'test notification content'
-                ], [
-                    'image' => 'https://static.vecteezy.com/system/resources/previews/009/394/762/original/bell-icon-transparent-notification-free-png.png',
-                    'title' => 'test',
-                    'content' => 'test notification content'
-                ], [
-                    'image' => 'https://static.vecteezy.com/system/resources/previews/009/394/762/original/bell-icon-transparent-notification-free-png.png',
-                    'title' => 'test',
-                    'content' => 'test notification content'
-                ], [
-                    'image' => 'https://static.vecteezy.com/system/resources/previews/009/394/762/original/bell-icon-transparent-notification-free-png.png',
-                    'title' => 'test',
-                    'content' => 'test notification content'
-                ], [
-                    'image' => 'https://static.vecteezy.com/system/resources/previews/009/394/762/original/bell-icon-transparent-notification-free-png.png',
-                    'title' => 'test',
-                    'content' => 'test notification content'
-                ], [
-                    'image' => 'https://static.vecteezy.com/system/resources/previews/009/394/762/original/bell-icon-transparent-notification-free-png.png',
-                    'title' => 'test',
-                    'content' => 'test notification content'
-                ],
-            ],
-        ];
+        $items = Notification::get();
 
-        // Return JSON response
-        return response()->json($data);
+        return response()->json(['data' => $items], 200);
     }
 }
